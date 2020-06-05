a7c583470c2c3d656fe71fbb605a93ef254ed9a7
RhettVX <reticentcool@protonmail.com>
2020-06-05 02:13:47 -0400

    Don't even ask

--- fl_inspector.py
+++ fl_inspector.py
@@ -1,5 +1,5 @@
 from pathlib import Path
-from typing import List, Union, Dict, Tuple
+from typing import List, Union, Dict, Tuple, Set
 import re
 
 from DbgPack.asset_manager import AssetManager, AbstractAsset
@@ -25,79 +25,158 @@ class ForgeLightGame:
         self.path = path
         self.asset_manager = AssetManager(list((path / self.assets_subpath).glob('*.pack*')))
 
-    # Return list of asset objects from the provided list of names/hashes
-    def get_assets(self, assets_names: List[Union[str, int]]) -> List[Tuple[str, AbstractAsset]]:
-        output_assets: List[Tuple[str, AbstractAsset]] = []
+    # Return asset object from the provided name/hash
+    # def get_asset(self, name: Union[str, int]) -> AbstractAsset:  #Tuple[str, AbstractAsset]:
+    #     if isinstance(name, str):
+    #         a = self.asset_manager[name]
+    #         if a:
+    #             return name, a
+    #         print(f'{name} not found.')
+    #
+    #     elif isinstance(name, int):
+    #         a = self.asset_manager[name]
+    #         if a:
+    #             return f'{name:#018x}.bin', a
+    #         print(f'{name:#018x} not found.')
+    #
+    #     else:
+    #         raise TypeError(f'get_assets() needs a filename or a filename hash. \'{type(name)}\' is not valid.')
 
-        for name in assets_names:
-            if isinstance(name, str):
-                a = self.asset_manager[name]
-                if not a:
-                    print(f'{name} not found.')
-                    continue
+    # FIXME: I am changing get_assets
+    # def extract_assets(self, asset_names: List[Union[str, int]]):
+    #     outdir = Path(f'./{self.name}_output')
+    #     outdir.mkdir(parents=True, exist_ok=True)
+    #
+    #     for asset in self.get_assets(asset_names):
+    #         (outdir / asset[0]).write_bytes(asset[1].get_data())
+    #
+    #     # for name in asset_names:
+    #     #     if isinstance(name, str):
+    #     #         a = self.asset_manager[name]
+    #     #         if not a:
+    #     #             print(f'Skipping "{name}"...')
+    #     #             continue
+    #     #
+    #     #         else:
+    #     #             (outdir / name).write_bytes(a.get_data())
+    #     #
+    #     #     elif isinstance(name, int):
+    #     #         a = self.asset_manager[name]
+    #     #         if not a:
+    #     #             print(f'Skipping "{name:#018x}"...')
+    #     #             continue
+    #     #
+    #     #         else:
+    #     #             (outdir / f'{name:#018x}.bin').write_bytes(a.get_data())
 
-                output_assets.append((name, a))
+    @staticmethod
+    def _scrape_asset(asset: AbstractAsset, pattern: str) -> Set[str]:
+        data = asset.get_data()
+        mo = re.findall(bytes(r'([><\w-]+\.(' + r'|'.join(pattern.split()) + r'))', 'utf-8'), data)
+        if mo:
+            # return [x[0].decode('utf8') for x in mo]
+            # name = m[0].decode('utf8')
+            return set({m[0].decode('utf8').casefold(): m[0].decode('utf8') for m in mo}.values())
+        return set()
 
-            elif isinstance(name, int):
-                a = self.asset_manager[name]
-                if not a:
-                    print(f'{name:#018x} not found.')
-                    continue
+    def trace_requisites(self, root_name: str, mode='zone'):
+        # Key will be the casefolded name. proper case is stored in the asset hopefully
+        completed_files: Dict[str, AbstractAsset] = {}
 
-                output_assets.append((f'{name:#018x}.bin', a))
+        # use casefold for lookup. value uses scraped case
+        skipped_files: Dict[str, str] = {}
+        file_queue: Dict[str, str] = {}
 
-            else:
-                raise TypeError(f'get_assets() needs a filename or a filename hash. \'{type(name)}\' is not valid.')
-
-        return output_assets
-
-    def extract_assets(self, asset_names: List[Union[str, int]]):
-        outdir = Path(f'./{self.name}_output')
-        outdir.mkdir(parents=True, exist_ok=True)
-
-        for asset in self.get_assets(asset_names):
-            (outdir / asset[0]).write_bytes(asset[1].get_data())
-
-        # for name in asset_names:
-        #     if isinstance(name, str):
-        #         a = self.asset_manager[name]
-        #         if not a:
-        #             print(f'Skipping "{name}"...')
-        #             continue
-        #
-        #         else:
-        #             (outdir / name).write_bytes(a.get_data())
-        #
-        #     elif isinstance(name, int):
-        #         a = self.asset_manager[name]
-        #         if not a:
-        #             print(f'Skipping "{name:#018x}"...')
-        #             continue
-        #
-        #         else:
-        #             (outdir / f'{name:#018x}.bin').write_bytes(a.get_data())
-
-    # TODO: Testing with zones first
-    # TODO: only take one name for now
-    # FIXME: This whole thing is sloppy
-    def trace_requisites(self, asset_names: List[str], mode='zone'):
-        completed_files: List[AbstractAsset] = []
-        file_queue: List[str] = []
-
-        zone_reqs = 'adr'.split()
-        adr_reqs = 'dme dma dds'
+        # zone_reqs = 'adr'
+        # adr_reqs = 'dme dma dds'
 
         # compile(bytes(r'([><\w-]+\.(' + r'|'.join(known_exts) + r'))', 'utf-8'))
 
-        for i, name in enumerate(asset_names):
-            if mode == 'zone':
-                # Initial scrape
-                data = self.get_assets([name])[0][1].get_data()
-                mo = re.findall(bytes(r'([><\w-]+\.(' + r'|'.join(zone_reqs) + r'))', 'utf-8'), data)
+        if mode == 'zone':
+            # Initial scrape
+            root_asset = self.asset_manager[root_name]
+            file_queue = {v.casefold(): v for v in self._scrape_asset(root_asset, 'adr')}
+            completed_files[root_name.casefold()] = root_asset
+            breakpoint()
 
-                if mo:
-                    for m in mo:
-                        print(m[0])
+            # root_asset = self.get_asset(root_name)
+            # file_queue = {v.casefold: v for v in self._scrape_asset(root_asset[1], 'adr')}
+            # completed_files[root_asset[0].casefold()] = root_asset[1]
+            #
+            # while file_queue:
+            #     name = file_queue.popitem()
+            #     if name[0] in completed_files or name[0] in skipped_files:
+            #         # We scraped this asset already or couldn't find it
+            #         continue
+            #
+            #     try:
+            #         asset = self.get_asset(name[1])
+            #
+            #     except KeyError:
+            #         print(f'{name[0]} not found. Skipping')
+            #         skipped_files[name[0]] = name[1]
+            #         continue
+            #
+            #     file_queue.update({asset[0]: self._scrape_asset(asset[1], 'dme dma dds')})
+
+            # if nameupper() in completed_files or name in skipped_files:
+                #     continue
+                #
+                # try:
+                #     asset = self.get_asset(name)
+                # except KeyError as e:
+                #     print(f'{name} not found. Skipping')
+                #     skipped_files.add(name)
+                #     continue
+                #
+                # file_queue.update(self._scrape_asset(asset[1], 'dme dma dds'))
+                # completed_files.append(asset[1])
+
+            # breakpoint()
+
+
+
+
+
+            # # Initial scrape
+            # asset = self.get_asset(name)
+            # if not asset:
+            #     raise KeyError(f'{name} not found. Aborting')
+            #
+            # data = asset[1].get_data()
+            # mo = re.findall(bytes(r'([><\w-]+\.(' + r'|'.join(zone_reqs.split()) + r'))', 'utf-8'), data)
+            #
+            # if mo:
+            #     for m in mo:
+            #         # print(m[0])
+            #         file_queue.append(m[0].decode('utf8'))
+            #
+            # completed_files.append(asset[1])
+            #
+            # # FIXME: Don't use <name>2
+            # # TODO: A lot of the initial scrape code can be function
+            # while file_queue:
+            #     name2 = file_queue.pop()
+            #     if name2 in [x.name for x in completed_files] or name2 in file_queue or name2 in skipped_files:
+            #         # We already scraped this asset so skip
+            #         continue
+            #
+            #     asset2 = self.get_asset(name2)
+            #     if not asset2:
+            #         print(f'{name2} not found. Skipping')
+            #         skipped_files.append(name2)
+            #
+            #     data2 = asset2[1].get_data()
+            #     mo2 = re.findall(bytes(r'([><\w-]+\.(' + r'|'.join(adr_reqs.split()) + r'))', 'utf-8'), data2)
+            #
+            #     if mo2:
+            #         for m2 in mo2:
+            #             test = m2[0].decode('utf8')
+            #             # if test == 'Esamir_Flora_Radial_IceRock03_LOD0.dme': breakpoint()
+            #             if test not in file_queue:
+            #                 print(test)
+            #                 file_queue.append(test)
+            #                 pass
 
 
 # def load_items(self):
@@ -133,7 +212,7 @@ if __name__ == '__main__':
 
     print('Tracing requisites...')
     # Nexus makes a good example because it is smaller and has some missing materials that I should be able to trace
-    ps2_test.trace_requisites(['Nexus.zone'])
+    ps2_test.trace_requisites('Nexus.zone')
 
     print('Finished')
     pass
